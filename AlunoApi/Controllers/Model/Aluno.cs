e3bd89462f313d1969f66f4295ef272b80930558
Wellington Henrique <wellingtonhlc@gmail.com>
2022-05-26 23:30:02 -0300

    Creating migration

---
+++ AlunoApi/Controllers/Model/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunoApi.Controllers.Model
+{
+    public class Aluno
+    {
+        //[Key] - Define uma chave primaria
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80, ErrorMessage ="")]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "")]
+        public string Email { get; set; }
+        [Required]
+        public int Idade { get; set; }
+    }
+}
